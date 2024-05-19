5ba65e610b1edcc3032debb10670f350b5e30e10
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-05-18 21:56:35 -0600

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { FormPage } from "./components/FormPage";
+import { FormPage } from "./components/formPage";
 
 function App() {
   return (
